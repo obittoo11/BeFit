5398c694e255470607ef8807164f6967d53d3765
Sahib Anand <sahibanand11@gmail.com>
2023-01-05 18:45:07 +0530

    Initial Commit

---
+++ BeFit/BeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitApp.swift
+//  BeFit
+//
+//  Created by Sahib Anand on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
